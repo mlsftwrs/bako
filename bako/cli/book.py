51c3cb9be6607c1ff8aafd4b815908735032fa0e
diarray-hub <yacoudiarra.wl@gmail.com>
2024-07-31 17:02:03 +0000

    Update Book insertion function | No more title column

--- bako/cli/book.py
+++ bako/cli/book.py
@@ -16,19 +16,18 @@ limitations under the License.
 import pandas as pd
 from bako.src.db.models import Book
 
-def df_to_book(df: pd.DataFrame):
+def df_to_book(book_tile: str, df: pd.DataFrame) -> Book:
     """Return a book object from a sheet dataframe
 
     Args:
         df (pd.DataFrame): The sheet dataframe
     """
-    title = df["title"][0]
-    content = content = {}
+    content: dict[str, list] = {}
     for idx, page_ref in df["Page No"].items():
         if page_ref in content:
             content[page_ref].append(df["RobotsMali adjusted-Review "][idx])
         else: content[page_ref] = [df["RobotsMali adjusted-Review "][idx]]
-    return Book(title=title, content=content)
+    return Book(title=book_tile, content=content)
 
 def add_books_to_mongodb(path_to_excel: str):
     """Adds new books to the book collection in our MongoDB deployment
@@ -39,6 +38,6 @@ def add_books_to_mongodb(path_to_excel: str):
     excel_file = pd.ExcelFile(path_to_excel)
     for story_book in excel_file.sheet_names:
         df = excel_file.parse(story_book)
-        book = df_to_book(df=df)
+        book = df_to_book(book_tile=story_book, df=df)
         book.create_doc()
     
